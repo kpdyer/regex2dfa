f830439946633b91ecac6af5469d9d78a777bfee
Kevin P. Dyer <kpdyer@gmail.com>
2014-06-02 09:16:01 -0700

    nearly-working regex -> dfa -> minimal dfa

--- src/regex2dfa.cc
+++ src/regex2dfa.cc
@@ -1,13 +1,9 @@
-#include <stdio.h>
-#include <getopt.h>
-
+#include <fstream>
 #include <iostream>
 
 #include "re2/re2.h"
 #include "re2/regexp.h"
 #include "re2/prog.h"
-
-int debug_flag, compile_flag, size_in_bytes;
 
 std::string attFstFromRegex( const std::string regex )
 {
@@ -46,33 +42,64 @@ std::string attFstFromRegex( const std::string regex )
     return retval;
 }
 
+std::string attFstMinimize(std::string str_dfa)
+{
+    std::string fst_path = "third_party/openfst/src/bin";
+    std::string retval;
+
+const char* temp_dir = getenv("TMPDIR");
+if (temp_dir == 0) {
+    temp_dir = "/tmp";
+}
+std::string temp_dir_str = std::string(temp_dir);
+std::string temp_file = "000000";
+    
+    std::string abspath_dfa     = temp_dir_str+ "/" + temp_file + ".dfa";
+    std::string abspath_fst     = temp_dir_str+ "/" + temp_file + ".fst";
+    std::string abspath_fst_min = temp_dir_str+ "/" + temp_file + ".min.fst";
+    std::string abspath_dfa_min = temp_dir_str+ "/" + temp_file + ".min.dfa";
+
+    // write our input DFA to disk
+    std::ofstream dfa_stream;
+    dfa_stream.open (abspath_dfa.c_str());
+    dfa_stream << str_dfa;
+    dfa_stream.close();
+
+    std::string cmd;
+
+    // convert our ATT DFA string to an FST
+    cmd = fst_path + "/fstcompile " + abspath_dfa + " " + abspath_fst;
+    system(cmd.c_str());
+
+    // convert our FST to a minmized FST
+    cmd = fst_path + "/fstminimize " + abspath_fst + " " + abspath_fst_min;
+    system(cmd.c_str());
+
+    // covert our minimized FST to an ATT FST string
+    cmd = fst_path + "/fstprint " + abspath_fst_min + " " + abspath_dfa_min;
+    system(cmd.c_str());
+
+    // read the contents of of the file at abspath_dfa_min to our retval
+    std::ifstream dfa_min_stream(abspath_dfa_min.c_str());
+    std::stringstream buffer;
+    buffer << dfa_min_stream.rdbuf();
+    retval = std::string(buffer.str());
+    dfa_min_stream.close();
+
+    // cleanup
+    remove( abspath_dfa.c_str() );
+    remove( abspath_fst.c_str() );
+    remove( abspath_fst_min.c_str() );
+    remove( abspath_dfa_min.c_str() );
+
+    return retval;
+}
 
 int
 main (int argc, char **argv)
 {
-std::cout << attFstFromRegex("^.+$");
-  // Invokes ctor `GetOpt (int argc, char **argv, 
-  //                       char *optstring);'
-  //GetOpt getopt (argc, argv, "dcs:");
-/*  char* regex;
-
-    int option_index = 0;
-  int option_char;
-    static struct option long_options[] =
-    {
-        {"regex",  required_argument, 0, 'r'},
-        {0, 0, 0, 0}
-    };
-  
-  // Invokes member function `int operator ()(void);'
-   while ((option_char = getopt_long (argc, argv, "d:i:",
-                long_options, &option_index)  ) !=-1)
-    switch (option_char)
-      {  
-         case 'r': regex = static_cast<char*>(optarg); break;
-         case '?': fprintf (stderr, 
-                            "usage: %s [dcs<size>]\n", argv[0]);
-      }
-*/
+  std::string dfa = attFstFromRegex("^.+$");
+  std::string minimized_dfa = attFstMinimize(dfa);
+  std::cout << minimized_dfa << std::endl;
 }
 
