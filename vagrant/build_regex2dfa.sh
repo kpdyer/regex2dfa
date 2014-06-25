626dfbbdfc833f074cc3d3ca204238119ce0b964
Kevin P. Dyer <kpdyer@gmail.com>
2014-06-25 11:57:16 -0700

    Issue #6

--- vagrant/build_regex2dfa.sh
+++ vagrant/build_regex2dfa.sh
@@ -2,6 +2,9 @@
 export NPMBUILD=1
 cd $BUILD_DIR
 git clone $GIT_REGEX2DFA
-cd regex2dfa
+cd $BUILD_DIR/regex2dfa/third_party/openfst
+emconfigure ./configure --enable-bin --disable-shared --enable-static
+emmake make
+cd $BUILD_DIR/regex2dfa
 emconfigure ./configure
 emmake make -j$CORES
